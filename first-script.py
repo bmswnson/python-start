d54883b7efeca88b1d2f4f320057006f2935a749
Ben Swanson <bmswnson@gmail.com>
2023-05-17 08:08:59 -0500

    Create first-script.py

---
+++ first-script.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a = 1
+a
+
+import pandas as ps
+
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello World")
+
+# Main program starts here
+hello()
+
+
+i = 42
+
+hello(i)
+
+a = ("a", "b", "c")
+
+test = 7
+
+test - 5
+
+#Number py calculations
+import numpy
+
+a = [1, 2, 3]
+
+#creates a numpy array
+b = numpy.array(a)
+
+#adds 10 to the 2nd and third numbers of the array
+b[1:3] += 10
+
+#other array functions
+a = numpy.array([1, 2, 3, 4]) 
+#subtracs every number in teh array
+
+#Using dictionaries with keys and come back here
+a = {‘banana’:1, ‘horse’:2, 19:1, 3:‘cookie’}
+
+print(a[‘banana’])
